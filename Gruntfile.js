207b51e1195d625853c9c99532f53a808097b4f0
Stephen Tvedt <stephen@stephentvedt.com>
2015-06-05 11:42:56 -0400

    open browser on grunt serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,7 +105,8 @@ module.exports = function(grunt) {
         server: {
           options: {
             port: 9001,
-            base: 'build'
+            base: 'build',
+            open: true
           }
         }
       }
